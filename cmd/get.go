2a683d553143455ebd3afeb9fabfbdbfd00dfef0
leonz <medgarxyz@gmail.com>
2025-07-24 16:38:53 -0600

    get implemented

--- cmd/get.go
+++ cmd/get.go
@@ -1,40 +1,39 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
+	"gitlab.com/0xleonz/gocv/internal/utils"
 )
 
-// getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Muestra los CVs configurados en ~/.config/gocv",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+		cfg := AppConfig // ya fue cargado en root
+
+		if len(cfg.Data.CVs) == 0 {
+			fmt.Println(utils.Colorize("⚠️  No hay CVs definidos en config.yml", utils.Yellow))
+			return
+		}
+
+		fmt.Println(utils.Colorize("📁 CVs configurados:\n", utils.Cyan))
+
+		for name, cv := range cfg.Data.CVs {
+			fmt.Printf("%s %s\n", utils.Colorize("•", utils.Purple), utils.Colorize(name, utils.Green))
+			fmt.Printf("  %s\n", utils.Colorize(cv.Description, utils.Yellow))
+			if t := cv.LastCompileTime(); t != nil {
+				fmt.Printf("  %s %s\n\n", utils.Colorize("🕒 Última compilación:", utils.Cyan), t.Format(time.RFC822))
+			} else {
+				fmt.Printf("  %s\n\n", utils.Colorize("🕒 Nunca compilado", utils.Red))
+			}
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(getCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
+
